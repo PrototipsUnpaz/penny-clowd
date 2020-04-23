24def07711a4dbf4e7d7d950b066862163b2aeb3
PrimSaphire <luz_oscurallive.com.ar>
2020-04-22 23:36:38 -0300

    Se agregó salto al personaje

--- scenes/player/Player.gd
+++ scenes/player/Player.gd
@@ -4,6 +4,9 @@ const SPEED = 300
 var velocity = Vector2()
 
 func _physics_process(delta):
+	velocity.y +=20
 	velocity.x = SPEED
+	if Input.is_action_pressed("Touch"):
+		velocity.y = -300
 	move_and_slide(velocity)
 	pass
