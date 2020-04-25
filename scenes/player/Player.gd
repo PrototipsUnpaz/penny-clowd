171300504d4c79421119d3f76c832c47c01be9d7
PrimSpahire <luz_oscura@live.com.ar>
2020-04-25 13:37:36 -0300

    update script personaje
    agregue la reproduccion de la animacion run al codigo

--- scenes/player/Player.gd
+++ scenes/player/Player.gd
@@ -6,6 +6,7 @@ var velocity = Vector2()
 func _physics_process(delta):
 	velocity.y +=20
 	velocity.x = SPEED
+	$AnimatedSprite.play("Correr")
 	if Input.is_action_pressed("Touch"):
 		velocity.y = -300
 	move_and_slide(velocity)
