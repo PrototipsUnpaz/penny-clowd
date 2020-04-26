8ec3f5810a1cc7e4c7f3e9e73ebee139cab1af59
cobregon89 <62080655+cobregon89@users.noreply.github.com>
2020-04-26 18:32:54 -0300

    Multisalto eliminado
    Hice que el personaje no repita el salto estando en el aire.  Además, le agregué craacterísticas a la plataforma para que se reconozca como piso.

--- scenes/player/Player.gd
+++ scenes/player/Player.gd
@@ -1,13 +1,16 @@
 extends KinematicBody2D
 
-const SPEED = 300
+const Speed = 150
 var velocity = Vector2()
 
 func _physics_process(delta):
 	velocity.y +=20
-	velocity.x = SPEED
+	velocity.x = Speed
+	move_and_slide(velocity,Vector2(0,-1))
+	if is_on_floor():
+		velocity.y = 0
 	$AnimatedSprite.play("Correr")
-	if Input.is_action_pressed("Touch"):
-		velocity.y = -300
+	if Input.is_action_pressed("Touch") and is_on_floor():
+		velocity.y = -600
 	move_and_slide(velocity)
 	pass
