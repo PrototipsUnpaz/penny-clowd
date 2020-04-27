a4d47ea4d611e9602c98f6ada408f05ab8736f6d
EnzoCostilla <52358924+enzocostilla@users.noreply.github.com>
2020-04-27 18:22:00 -0300

    objetos_enemigos

---
+++ scenes/enemigos/circulo_fuego/curculo_fuego.gd
@@ -0,0 +1,8 @@
+extends AnimatedSprite
+
+func _ready():
+	pass
+
+
+func _on_Area2D_area_entered(area):
+	pass # Replace with function body.
---
+++ scenes/enemigos/cono_enemigo/cono_enemigo.gd
@@ -0,0 +1,21 @@
+extends Sprite
+
+
+func _physics_process(delta):
+	
+	
+	#if(get_slide_collision(get_slide_count()-1) != null):
+		#var obj_colisionador = get_slide_collision(get_slide_count()-1).collider
+		#if (obj_colisionador.is_in_group("jugardo")):
+			#obj_colisionador.queue_free()
+	
+			
+	pass
+
+
+
+
+
+
+func _on_Area2D_area_entered(area):
+	pass # Replace with function body.
