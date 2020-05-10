1e7c796878f67e83c74d94030c9d11646bf635d8
EnzoCostilla <52358924+enzocostilla@users.noreply.github.com>
2020-05-10 00:20:00 -0300

    instancia-circulo-fuego-enemigo

--- Position2D.gd
+++ Position2D.gd
@@ -2,7 +2,10 @@ extends Position2D
 
 
 var conoenemigo = preload("res://escenas/enemigos/cono_enemigo/ConoEnemigo.tscn")
+
 var paredenemigo = preload("res://escenas/enemigos/pared_enemigo/ParedEnemigo.tscn")
+
+var circuloenemigo =  preload("res://escenas/enemigos/circulo_fuego/CirculoFuego.tscn")
 
 export (String, "cono", "pared" ,"circulo") var obstaculo = "cono"
 
@@ -16,6 +19,10 @@ func _on_VisibilityNotifier2D_screen_entered():
 	if obstaculo == "pared":
 		var instaciapared = paredenemigo.instance()
 		add_child(instaciapared)
+	
+	if obstaculo == "circulo":
+		var instaciacirculo = circuloenemigo.instance()
+		add_child(instaciacirculo)
 	pass 
 
 
