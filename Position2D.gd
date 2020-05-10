186b00912a101a9fc06e94dba9a24d557a7ce1c4
EnzoCostilla <52358924+enzocostilla@users.noreply.github.com>
2020-05-10 00:13:09 -0300

    instanciapared-enemigo

--- Position2D.gd
+++ Position2D.gd
@@ -2,6 +2,7 @@ extends Position2D
 
 
 var conoenemigo = preload("res://escenas/enemigos/cono_enemigo/ConoEnemigo.tscn")
+var paredenemigo = preload("res://escenas/enemigos/pared_enemigo/ParedEnemigo.tscn")
 
 export (String, "cono", "pared" ,"circulo") var obstaculo = "cono"
 
@@ -11,6 +12,10 @@ func _on_VisibilityNotifier2D_screen_entered():
 	if obstaculo == "cono":
 		var instanciaCono = conoenemigo.instance()
 		add_child(instanciaCono)
+		
+	if obstaculo == "pared":
+		var instaciapared = paredenemigo.instance()
+		add_child(instaciapared)
 	pass 
 
 
