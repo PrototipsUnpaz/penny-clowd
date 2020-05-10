afbe250bd59f2462dad7a5a8c52caa285258d93d
EnzoCostilla <52358924+enzocostilla@users.noreply.github.com>
2020-05-09 23:24:05 -0300

    areglando-errores

--- Position2D.gd
+++ Position2D.gd
@@ -1,14 +1,11 @@
 extends Position2D
 
 
-var conoenemigo = preload ("res://escenas/enemigos/cono_enemigo/ConoEnemigo.gd")
-var visual = false
-export (String, "cono", "pared" ,"circulo") var obstaculo
+
 
 
 func _on_VisibilityNotifier2D_screen_entered():
 	print("visualizado position2d")
-	
 	pass 
 
 
