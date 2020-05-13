1f3c0fcb3ac74e78f15c752f3758e07bb3d31ec5
PrimSpahire <luz_oscura@live.com.ar>
2020-05-13 00:36:51 -0300

    update item

--- escenas/items/Item.gd
+++ escenas/items/Item.gd
@@ -2,9 +2,8 @@ extends Node2D
 
 
 func _on_Area2D_area_entered(area):
-	Vidas.suma_items()
-	Vidas.sumar_vida()
-	queue_free()
-	#SonidoItem.play()
-#la línea anterior es para dar sonido a cada item
-	pass 
+		Vidas.suma_items()
+		Vidas.sumar_vida()
+		#$SonidoItem.play()
+		queue_free()
+		pass 
