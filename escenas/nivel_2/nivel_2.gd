b73772a699a98517398202868d9893b6fc68d202
EnzoCostilla <52358924+enzocostilla@users.noreply.github.com>
2020-05-22 17:53:48 -0300

    diabolo_g_interacciones_hechos

--- escenas/nivel_2/nivel_2.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-var check_nivel2 = false
-var nivel_2
-func _on_Area2D_area_entered(area):
-	check_nivel2 = true
-	if check_nivel2 == true:
-		nivel_2 = 2
-	pass # Replace with function body.
