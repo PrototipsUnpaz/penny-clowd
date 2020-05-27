dda7960ceb2638f14baecba1d67162b872681285
EnzoCostilla <52358924+enzocostilla@users.noreply.github.com>
2020-05-27 17:15:52 -0300

    diabolos-funcionales

--- escenas/enemigos/diabolo_g/DiaboloG.gd
+++ escenas/enemigos/diabolo_g/DiaboloG.gd
@@ -6,14 +6,14 @@ var velocity = Vector2()
 
 func _physics_process(delta):
 
-	if global_position.y < 570:
+	if global_position.y < 335:
 		velocity.y += 10
-	if global_position. y >=570:
-		position. y =570
+	if global_position. y >=335:
+		position. y =335
 		velocity. y = 0
 	velocity.y +=20
 	velocity.x = speed
-	if global_position. y ==570:
+	if global_position. y ==335:
 		#$AnimatedSprite.play("diabolo_m")
 		velocity.y = 0
 	move_and_slide(velocity,Vector2(1000,0))
