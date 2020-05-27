307ff2c02cff675944cbc1ed2be472da87b46f7b
EnzoCostilla <52358924+enzocostilla@users.noreply.github.com>
2020-05-27 17:16:21 -0300

    nivel 2 errores solucionados

---
+++ escenas/nivel_2/Nivel2.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+func _ready():
+	Vidas.nivelDos = true
+	Vidas.posPenny2 = true
+	pass
+	
+func _process(delta):
+	$Ui/UIVidas/TextoVida. text="x "+String(Vidas.vidasNivel)
+	pass
