0c4a10d6feb3c1e0ea04aac895554415a6d89c0e
cobregon <62080655+cobregon89@users.noreply.github.com>
2020-06-07 17:57:02 -0300

    Programación de splash screen

--- escenas/splash_screen/SplashScreen.gd
+++ escenas/splash_screen/SplashScreen.gd
@@ -1,7 +1,13 @@
 extends Node2D
 
 func _ready():
+	$Timer.start()
+	pass
+
+func ir_al_menu_principal():
+	get_tree().change_scene("res://escenas/menu_principal/MenuPrincipal.tscn")
 	pass
 
 func _on_Timer_timeout():
+	ir_al_menu_principal()
 	pass
