b672efce5acb7597aec32943b79b7c63a8cc4d1e
PrimSpahire <luz_oscura@live.com.ar>
2020-06-07 16:28:39 -0300

    detalles extras

--- escenas/enemigos/diabolo_m/DiaboloM.gd
+++ escenas/enemigos/diabolo_m/DiaboloM.gd
@@ -23,8 +23,8 @@ func _physics_process(delta):
 func _on_Area2D_area_entered(area):
 	Vidas.quitar_vida()
 	queue_free()
-	get_tree().change_scene("res://escenas/pantalla_negra/PantallaNegra.tscn")
-	if Vidas.vidasNivel == -1:
-		queue_free()
+	if Vidas.vidasNivel == 0:
 		get_tree().change_scene("res://escenas/fin_del_juego/FinDelJuego.tscn")
+	else:
+		get_tree().change_scene("res://escenas/pantalla_negra/PantallaNegra.tscn")
 	pass
--- escenas/enemigos/pared_enemigo/ParedEnemigo.gd
+++ escenas/enemigos/pared_enemigo/ParedEnemigo.gd
@@ -4,10 +4,10 @@ extends Sprite
 func _on_Area2D_area_entered(area):
 	Vidas.quitar_vida()
 	queue_free()
-	get_tree().change_scene("res://escenas/pantalla_negra/PantallaNegra.tscn")
-	if Vidas.vidasNivel == -1:
-		queue_free()
+	if Vidas.vidasNivel == 0:
 		get_tree().change_scene("res://escenas/fin_del_juego/FinDelJuego.tscn")
+	else:
+		get_tree().change_scene("res://escenas/pantalla_negra/PantallaNegra.tscn")
 	pass
 
 
