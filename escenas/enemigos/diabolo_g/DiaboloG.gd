6cd49097a7898944651b39fac5c0a4a1a81fdec6
cobregon <62080655+cobregon89@users.noreply.github.com>
2020-06-07 02:17:04 -0300

    Fin por diábolo grande
    El diábolo grande lleva a la escena de fin de juego

--- escenas/enemigos/diabolo_g/DiaboloG.gd
+++ escenas/enemigos/diabolo_g/DiaboloG.gd
@@ -25,4 +25,7 @@ func _on_Area2D_area_entered(area):
 	Vidas.quitar_vida()
 	queue_free()
 	get_tree().change_scene("res://escenas/pantalla_negra/PantallaNegra.tscn")
+	if Vidas.vidasNivel == -1:
+		queue_free()
+		get_tree().change_scene("res://escenas/fin_del_juego/FinDelJuego.tscn")
 	pass
