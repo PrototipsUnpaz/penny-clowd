ec34212c95be9f4f6776e271d10feb432f49eaa0
cobregon <62080655+cobregon89@users.noreply.github.com>
2020-06-07 23:32:22 -0300

    Contenido del script de transición 2

--- escenas/transicion_nivel_2/TransicionNivel2.gd
+++ escenas/transicion_nivel_2/TransicionNivel2.gd
@@ -1,20 +1,13 @@
 extends Node2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$Timer.start()
+	pass
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
+func ir_al_nivel_2():
+	get_tree().change_scene("res://escenas/nivel_2/Nivel2.tscn")
+	pass
+	
 func _on_Timer_timeout():
-	pass # Replace with function body.
+	ir_al_nivel_2()
+	pass
