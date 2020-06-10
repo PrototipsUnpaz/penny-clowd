ba95bc8e1e189b7ced17e87f6ec15494e111b079
PrimSpahire <luz_oscura@live.com.ar>
2020-06-10 20:53:34 -0300

    retoques para salir a produccion

--- escenas/nivel_2/Nivel2.gd
+++ escenas/nivel_2/Nivel2.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 func _ready():
+	$Timer.start()
 	$Soundtrack.play()
 	Vidas.nivelDos = true
 	Vidas.posPenny2 = true
@@ -9,3 +10,15 @@ func _ready():
 func _process(delta):
 	$Ui/UIVidas/TextoVida. text="x "+String(Vidas.vidasNivel)
 	pass
+
+
+func _on_Salir_button_down():
+	$SonidoBoton.play()
+	get_tree().quit()
+	pass 
+
+
+func _on_Timer_timeout():
+	$gatcies/Salir.visible = true
+	$gatcies/Label.visible = true
+	pass 
