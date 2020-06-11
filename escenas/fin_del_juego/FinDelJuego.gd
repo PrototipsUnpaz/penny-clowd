60fb4a713b9117bdc6186e0b859e21c89960822b
PrimSpahire <luz_oscura@live.com.ar>
2020-06-11 15:04:18 -0300

    fixed items

--- escenas/fin_del_juego/FinDelJuego.gd
+++ escenas/fin_del_juego/FinDelJuego.gd
@@ -20,5 +20,12 @@ func _on_Button2_button_down():
 func _on_Button_button_down():
 	$Boton.play()
 	Vidas.vidasNivel = 3
+	Vidas.items = 0
+	Vidas.itemLike = false
+	Vidas.itemLove = false
+	Vidas.itemSmile = false
+	Vidas.quitarItemLike = false
+	Vidas.quitarItemLove = false
+	Vidas.quitarItemSmile = false
 	get_tree().change_scene("res://escenas/menu_principal/MenuPrincipal.tscn")
 	pass 
